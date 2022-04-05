d1414b945afe4b631f06f9f7bf5e1561eff320de
bsal cgdl <bishalsigdel885@gmail.com>
2022-04-05 20:34:45 +0545

    completed all the tasks, homeworks and coding challenges

---
+++ LAB4/Two.activity.challenge/app/src/main/java/com/example/twoactivitychallenge/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.twoactivitychallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE= "";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void passageOne(View view) {
+        Log.d(LOG_TAG, "Button Clicked!");
+        Intent intent = new Intent(this, SecondActivity.class);
+        String passage = this.getResources().getString(R.string.passage_one);
+        intent.putExtra(EXTRA_MESSAGE, passage);
+        startActivity(intent);
+    }
+
+    public void passageTwo(View view) {
+        Log.d(LOG_TAG, "Button Two Clicked!");
+        Intent intent = new Intent(this, SecondActivity.class);
+        String passage = this.getResources().getString(R.string.passage_two);
+        intent.putExtra(EXTRA_MESSAGE, passage);
+        startActivity(intent);
+    }
+
+    public void passageThree(View view) {
+        Log.d(LOG_TAG, "Button Third Clicked!");
+        Intent intent = new Intent(this, SecondActivity.class);
+        String passage = this.getResources().getString(R.string.passage_third);
+        intent.putExtra(EXTRA_MESSAGE, passage);
+        startActivity(intent);
+    }
+}
